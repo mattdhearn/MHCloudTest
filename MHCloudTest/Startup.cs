52c84d64f88065265e963c7130ae07b05b7dc676
mattdhearn <matthew.hearn@digital.justice.gov.uk>
2016-07-15 14:22:02 +0100

    Initial commit

---
+++ MHCloudTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MHCloudTest.Startup))]
+namespace MHCloudTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
